834789f2ab4f580f0fc02c380fc0710bd673c392
waka-man <wtwestman@gmail.com>
2022-08-10 13:58:23 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a team project, making our own version of printf, a variadic function that prints formatted string on stdout
---
+++ _printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _printf - similar like printf, prints formatted string to stdout
+ * based on format specifier, which is the first parameter for this
+ * variadic function
+ * @format: a pointer to the first character in the first string
+ * parameter
+ * Return: number of chars processed, negative number (-1 in our case)
+ * on failure
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list arg; /** pointer to first argument **/
+	int chars; /** variable to hold number of chars scanned **/
+
+	
+}
