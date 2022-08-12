2bcb96586b613429c67496396cc174d509459b68
Nat-251 <natygizachew@gmail.com>
2022-08-12 16:14:15 -0700

    add more files

--- _printf.c
+++ _printf.c
@@ -1,41 +1,90 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
 /**
- * _printf - similar like printf, prints formatted string to stdout
- * based on format specifier, which is the first parameter for this
- * variadic function
- * @format: a pointer to the first character in the first string
- * parameter
- * Return: number of chars processed, negative number (-1 in our case)
- * on failure
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
  */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
 
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			  unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+						&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-	va_list arg; /** pointer to first argument **/
-	int chars; /** variable to hold number of chars scanned **/
-
-	format_t specifiers[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_dec},
-		{"i", print_dec},
-		{NULL, NULL}
-	};
+	buffer_t *output;
+	va_list args;
+	int ret;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
-	/** Get args one by one and pass them to the printer getter function **/
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
-	va_start(arg, format);
-	chars = get_printer(format, specifiers, arg);
+	va_start(args, format);
 
-	va_end(arg);
+	ret = run_printf(format, args, output);
 
-	return (chars);
+	return (ret);
 }
